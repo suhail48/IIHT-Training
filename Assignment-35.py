249fa8ba1f7387eb7eaa40b4522d0dbb098c22b8
Mohd Suhail <mailme.suhail48@gmail.com>
2021-09-29 10:58:18 +0530

    Create Assignment-35.py

---
+++ Assignment-35.py
@@ -0,0 +1,14 @@
+#Append element to list
+x = lambda l, ele : l.append(ele)
+l=[4,2,98,6,1,7]
+e = int(input("Enter element to append to list: "))
+x(l, e)
+print("list: ",l)
+
+#Convert list to dictionary
+
+new_dict = { l[i]:l[i] for i in range(len(l))}
+print("List converted to dictionary: ",new_dict)
+
+#sort dictionary based on values
+dict(sorted(new_dict.items(), key=lambda item: item[1]))
